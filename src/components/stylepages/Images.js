aa35407cf24bab95560960f14b8c0b56d9bb235f
mofilof <44873229+mofilof@users.noreply.github.com>
2020-04-26 01:52:32 +0200

    Images page updated

--- src/components/stylepages/Images.js
+++ src/components/stylepages/Images.js
@@ -10,44 +10,40 @@ function Images() {
   return (
     <>
       <main>
+        <h1>Image usage</h1>
+        <p>This carousel contains three images that work well with a small quote or alone. Why? Because they have one main motive and a calm background without to many distracting elements. "Landscape" images work great on desktops while "portraits" are great on mobile phones.</p>
+
         <Carousel>
           <Carousel.Item>
             <img className="d-block w-100"
-              src={Lift} alt="Close up ice skate photo" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+              src={Lift} alt="Ski lift." />
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100"
-              src={Skiier} alt="Close up ice skate photo" />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+              src={Skiier} alt="Alpine skier." />
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100"
-              src={Hockey} alt="Close up ice skate photo" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
+              src={Hockey} alt="Hockey player." />
           </Carousel.Item>
         </Carousel>
 
         <h1>The winter sports club, style guide</h1>
-        <img className="d-block portrett"
-          src={Skates} alt="Close up ice skate photo" />
-        <img className="d-block landskap"
-          src={Lift} alt="Close up ice skate photo" />
-        <img className="d-block landskap"
-          src={Skiier} alt="Close up ice skate photo" />
-        <img className="d-block landskap"
-          src={Hockey} alt="Close up ice skate photo" />
+        <div className="row">
+          <div className="col-sm-5">
+            <img className="d-block portrett"
+              src={Skates} alt="Close up ice skate photo" />
+          </div>
+          <div className="col">
+            <h2>Detail images</h2>
+            <p>Close up images, are easy to asociate with something. Even if they only show one piece not a situation. </p>
+
+            <p>Images with a spesific detail like skates or skis can be used as links. This image (left) could for examle link to an article about a equipment swap/ second hand events arranged by the club. </p>
+
+            <p>Instead of using images or buttons to redirect or illustrate something in an article it is a good idea to use the clubs official pictograms. They are a group of stylized icons.</p>
+          </div>
+        </div>
+
       </main>
 
     </>
