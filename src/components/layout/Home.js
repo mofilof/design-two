044d5379c53bff4ef708651ff4186f5a2549fefd
mofilof <44873229+mofilof@users.noreply.github.com>
2020-04-26 23:59:03 +0200

    Update Home.js

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -7,6 +7,19 @@ function Home() {
       <main>
         <h1>The winter sports club, style guide</h1>
         <p><b>A style guide to help all of our company have a similar look.</b></p>
+
+        <h2>Categories:</h2>
+        <ul>
+          <li>
+            <h3>Fonts</h3>
+          </li>
+          <li>
+            <h3>Colors</h3>
+          </li>
+          <li>
+            <h3>Images</h3>
+          </li>
+        </ul>
       </main>
     </>
   );
